a80774abc86bc53e14550f142c7eb8b70fbba629
Josh Birch <joshuabirch@protonmail.com>
2025-11-25 09:56:00 +0000

    Add further notes on macros

--- macros.cpp
+++ macros.cpp
@@ -20,5 +20,6 @@ int main()
     cout << RULE << endl << BOOK << endl << RULE;
     cout << endl << "NUM is: " << NUM << endl;
     cout << "Double NUM: " << ((NUM ) * 2) << endl;
+
     return 0;
 }
